095fbb590c7c51b6639875e219bec4017b8db8dd
SSH1van <akaunt654321@gmail.com>
2025-01-19 12:12:41 +0300

    Returned: Cookie upload

--- src/main/java/com/ivan/selenium/ozonparser/Main.java
+++ src/main/java/com/ivan/selenium/ozonparser/Main.java
@@ -36,6 +36,9 @@ public class Main {
                 // Открываем ссылку на товар
                 actions.openUrl(url, timeSleep);
 
+                // Загрузка куков из файла
+                WebDriverManager.loadCookies("cookies.json");
+
                 // Проверка на наличие экрана блокировки
                 if (actions.checkLockScreen()) return;
 
